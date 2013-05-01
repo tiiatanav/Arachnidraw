eb53a0f5337d3332d417fdd2dd2bf5dab1fd71b1
Tiia Tänav <tiiatanav@hotmail.com>
2013-05-01 15:02:54 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Arachnidraw::Application.config.secret_token = '4e7d1d8a1ed028985b3a9c36a13c2c7fc110db6ff156fa928357a3c98d2c817c03f8b615f40ee8437835ef512340e7c8a4aa3a4a2f05cfc1b9a1d0e784588672'
